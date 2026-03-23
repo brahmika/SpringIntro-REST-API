41596fbf71478ab5d031236747956458dfb9b4f5
brahmika <brahmika30@gmail.com>
2026-03-24 00:14:48 +0530

    [Brahmika] Added: UC2 - GET /hello/query with name as query parameter.

--- src/main/java/com/bridgelabz/HelloRestAPI/HelloController.java
+++ src/main/java/com/bridgelabz/HelloRestAPI/HelloController.java
@@ -1,14 +1,21 @@
 package com.bridgelabz.HelloRestAPI;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    // UC1 - Simple GET request
+    // UC1 - Simple GET
     @GetMapping("/hello")
     public String hello() {
         return "Hello from BridgeLabz";
+    }
+
+    // UC2 - GET with Query Parameter
+    @GetMapping("/hello/query")
+    public String helloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
